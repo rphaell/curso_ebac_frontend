54d6b1afa4c68712b75d66d33c6f394696009c23
Raphael <phael92br@gmail.com>
2024-05-07 03:11:36 -0300

    initial commit

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
-// Selecionando elementos e armazenando em variáveis
-const form = document.querySelector('form');
-const numeroA = document.getElementById('numeroA');
-const numeroB = document.getElementById('numeroB');
-const successMessage = document.querySelector('.sucess-message');
-const errorMessage = document.querySelector('.error-message');
-
-//  função para "escutar" e lidar com a submissão do formulário, executando as funções dentro quando o botão "Calcular" é clicado.
-form.addEventListener('submit', function(e) {
-    // previne o comportamento padrão de envio do formulário
+$('form').on('submit', function (e) {
     e.preventDefault();
-    
 
-    // Converte os valores dos campos de entrada para float para garantir de que um número será usado no calculo
-    const numberA = parseFloat(numeroA.value);
-    const numberB = parseFloat(numeroB.value);
+    const inputTarefaNova = $('#input-tarefa-nova').val();
+    const tarefaNova = $('<li style="display:none"></li>');
 
-    // Verifica se o número B é maior que o número A
-    if (numberB > numberA) {
-        // moistra a mensagem de sucesso e esconde a mensagem de erro
-        successMessage.textContent = "Formulário válido";
-        successMessage.style.display = 'block';
-        errorMessage.style.display = 'none';
-    } else {        
-        // mostra a mensagem de erro e esconde a mensagem de sucesso
-        errorMessage.textContent = "Formulário inválido";
-        errorMessage.style.display = 'block';
-        successMessage.style.display = 'none';
-    }
-});
+    $(`<li>${inputTarefaNova}</li>`).appendTo(tarefaNova);
+    $(tarefaNova).appendTo('ul');
+    $(tarefaNova).fadeIn(500);
+    $('#input-tarefa-nova').val('')
+
+    //fazer a linha na tarefa concluida quando clicada
+    $(tarefaNova).click(function(){
+        $(this).toggleClass('line-through')
+    })
+})
