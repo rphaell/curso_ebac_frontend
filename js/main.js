fb1eb90aeb35fa1fd507ca67b4b2a7e4620db876
Raphael <phael92br@gmail.com>
2024-05-07 23:11:29 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){
+
+
+    //mascara telefone
+    $('#telefone').mask('(00) 00000-0000')
+
+    //mascara CPF
+    $('#cpf').mask('000.000.000-00')
+
+    //mascara CEP
+    $('#cep').mask('00000-000')
+
+})
